686d530112dada6679291ea531df62359e29045e
Jefferson <dez.jefferson2@gmail.com>
2019-12-04 17:56:44 -0300

    Camada Aplicação

---
+++ ProjetoModeloDDD.Domain/Services/ClienteService.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces;
+using ProjetoModeloDDD.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Domain.Services
+{
+    public class ClienteService : ServiceBase<Cliente>, IClienteService
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteService(IClienteRepository clienteRepository)
+            :base(clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        public IEnumerable<Cliente> ObterClientesEspeciais(IEnumerable<Cliente> clientes)
+        {
+            //Metodo da classe Clientes (Essa classe recebe uma lista de clientes, e é devolvido só os especiais)
+            return clientes.Where(c => c.ClienteEspecial(c));
+        }
+    }
+}
